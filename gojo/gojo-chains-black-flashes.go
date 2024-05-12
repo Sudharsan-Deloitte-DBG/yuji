b9a062e148dc81ba441105f0f1f0298a49a87e9a
Sudharsan-Deloitte-DBG <160199554+Sudharsan-Deloitte-DBG@users.noreply.github.com>
2024-05-12 19:13:28 +0530

    hollow purple

--- gojo/gojo-chains-black-flashes.go
+++ gojo/gojo-chains-black-flashes.go
@@ -3,3 +3,7 @@ package gojo
 func GojoChainsBlackFlashes() string {
 	return "Gojo can chain black flashes, not as much as yuji though as he is blessed by the gods of black flash"
 }
+
+func HollowPurple() string {
+	return "Too strong!! Universe destroyed!!"
+}
