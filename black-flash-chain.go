0df8cca35434a28cd4ff73045f69d6fd1f857b6c
Sudharsan-Deloitte-DBG <160199554+Sudharsan-Deloitte-DBG@users.noreply.github.com>
2024-05-12 18:05:02 +0530

    print chained black flashes

--- black-flash-chain.go
+++ black-flash-chain.go
@@ -1,12 +1,15 @@
 package yuji
 
 import (
+	"fmt"
 	"math/rand"
+	"strconv"
 
 	"github.com/Sudharsan-Deloitte-DBG/sukuna"
 )
 
 func ChainBlackFlash() string {
 	i := rand.Intn(9)
+	fmt.Println("Black flash chained " + strconv.Itoa(i) + "times")
 	return sukuna.DefeatSukuna(i)
 }
